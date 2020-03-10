6bcf2e0e4a87b6ede49c6a914b96dcaab6f56d44
fvitalini <francesca.vitalini@mirai-solutions.com>
2020-03-10 10:59:34 +0100

    remove utils

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' Inverted versions of in, is.null and is.na
+#'
+#' @noRd
+#'
+#' @examples
+#' 1 %not_in% 1:10
+#' not_null(NULL)
+`%not_in%` <- Negate(`%in%`)
+
+not_null <- Negate(is.null)
+
+not_na <- Negate(is.na)
